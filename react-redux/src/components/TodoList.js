6914cf751465b906cfc6dc32110fb752f82cc3a4
zmj0920 <mj960920@163.com>
2019-07-06 17:37:34 +0800

    update

--- react-redux/src/components/TodoList.js
+++ react-redux/src/components/TodoList.js
@@ -35,7 +35,6 @@ class TodoList extends Component {
               width: '250px',
               marginRight: '10px'
             }}
-
           />
           <Button type="primary" >增加</Button>
         </div>
@@ -45,8 +44,63 @@ class TodoList extends Component {
             dataSource={this.state.userList}
             renderItem={(item, index) => (<List.Item>{item.uname}</List.Item>)}
           />
-        </div>
-      </div>
+          <table>
+            <thead>
+              <tr>
+                <th>登录名称</th>
+                <th>电子邮箱</th>
+                <th>联系方式</th>
+                <th>真实姓名</th>
+                <th>用户性别</th>
+                <th>操作</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                this.state.userList.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td >{item.uname}</td>
+                      <td>{item.email}</td>
+                      <td>{item.phone}</td>
+                      <td>{item.user_name}</td>
+                      <td>{item.gender}</td>
+                      <td>
+                        {/* <a href="#" >删除</a>
+                        <a href="#">修改</a> */}
+                        <Button>删除</Button>
+                        <Button>修改</Button>
+                      </td>
+                    </tr>
+                  )
+                })
+              }
+
+            </tbody>
+          </table>
+
+
+
+
+
+
+
+
+
+
+          {/* <Table className="table-item" title={() => '商品列表'} dataSource={this.state.userList} bordered  >
+            <Column title="编号" dataIndex="uid" key="uid" />
+            <Column title="姓名" dataIndex="uname" key="uname" />
+            <Column title="电子邮箱" dataIndex="email" key="email" />
+            <Column title="联系方式" dataIndex="phone" key="phone" />
+            <Column title="真实姓名" dataIndex="user_name" key="user_name" />
+            <Column title="用户性别" dataIndex="gender" key="gender" />
+          </Table> */}
+
+
+
+        </div >
+      </div >
     );
   }
 }
